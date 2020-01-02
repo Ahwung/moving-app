45ba319101cb3f53ef1c9d4384ff16b9957e6d19
Adam Hwung <Ahwung@gmail.com>
2020-01-02 13:19:35 -0800

    set up views for App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@
 // Packages
 import React from 'react';
 
+// Bootstrap stylesheet
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 // Components
 import Header from './components/Header.js'
 import Aside from './components/Aside.js'
@@ -14,7 +17,71 @@ import Navigation from './components/Navigation.js'
 // COMPONENT CLASS
 // =============================
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      view: {
+        page: 'homepage',
+        pageTitle: 'All Products'
+      },
+      formInputs: {
+        id: null,
+        name: null,
+        price: null,
+        url: null,
+        store: null,
+        room: null,
+        category: null,
+        bought: null
+      }
+    }
+  }
 
+  handleView = (view, productData) => {
+    let pageTitle =  ''
+    let formInputs = {
+      id: null,
+      name: '',
+      price: '',
+      url: '',
+      store: '',
+      room: '',
+      category: '',
+      bought: ''
+    }
+
+    switch (view) {
+      case 'homepage':
+        pageTitle = 'All Products'
+        break
+      case 'addProduct':
+        pageTitle = 'Add New Product'
+        break
+      case 'editProduct':
+        pageTitle = 'Edit Product'
+        formInputs = {
+          id: productData.id,
+          name: productData.name,
+          price: productData.price,
+          url: productData.url,
+          store: productData.store,
+          room: productData.room,
+          category: productData.category,
+          bought: productData.bought
+        }
+        break
+      default:
+        break
+    }
+
+    this.setState ({
+      view: {
+        page: view,
+        pageTitle: pageTitle
+      },
+      formInputs: formInputs
+    })
+  }
 
   render () {
     return (
---
+++ src/components/Form.js
@@ -0,0 +1,21 @@
+// =============================
+// DEPENDENCIES
+// =============================
+// Packages
+import React from 'react';
+
+// =============================
+// COMPONENT CLASS
+// =============================
+class Form extends React.Component {
+    render () {
+        return (
+            s
+        )
+    }
+}
+
+// =============================
+// EXPORT
+// =============================
+export default Form;
