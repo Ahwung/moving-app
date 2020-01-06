8143254fc4dd7b158d56a8ec8f06226dc892c6e6
Adam Hwung <Ahwung@gmail.com>
2020-01-05 23:14:12 -0800

    setting up table view

---
+++ src/components/Grid.js
@@ -0,0 +1,23 @@
+// =============================
+// DEPENDENCIES
+// =============================
+// Packages
+import React from 'react';
+
+// =============================
+// COMPONENT CLASS
+// =============================
+class Grid extends React.Component {
+    render () {
+        return (
+            <aside>
+                This is the Grid component
+            </aside>
+        )
+    }
+}
+
+// =============================
+// EXPORT
+// =============================
+export default Grid;
