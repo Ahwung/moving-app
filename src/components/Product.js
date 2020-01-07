7ed496c60f6860f8c28c0deb59db4ac491ece497
Adam Hwung <Ahwung@gmail.com>
2020-01-07 15:01:11 -0800

    added links to pages and styling for individual cards

--- src/components/Product.js
+++ src/components/Product.js
@@ -15,20 +15,18 @@ import './Product.css'
 class Product extends React.Component {
     render () {
 
-        // let borderColor;
-        // if (this.props.productData.bought === "f") {
-        //     this.props.productData.bought = "false"
-        //     borderColor = "dark"
-        // } else if (this.props.productData.bought === "t") {
-        //     this.props.productData.bought = "true"
-        //     borderColor = "primary"
-        // }
+        let borderColor;
+        if (this.props.productData.bought === "False") {
+            borderColor = "dark"
+        } else if (this.props.productData.bought === "True") {
+            borderColor = "primary"
+        }
 
         return (
             <div className="product-container">
-                <Card border="dark" style={{width: '18rem'}} className="card">
+                <Card border={borderColor} style={{width: '18rem'}} className="card">
                     <Card.Body>
-                        <Card.Title>{this.props.productData.name}</Card.Title>
+                        <Card.Title><a href={this.props.productData.url} target="_blank" rel="noopener noreferrer">{this.props.productData.name}</a></Card.Title>
                         <Card.Text>
                             Price: ${this.props.productData.price}
                         </Card.Text>
@@ -40,8 +38,8 @@ class Product extends React.Component {
                         <ListGroup.Item>Bought: {this.props.productData.bought}</ListGroup.Item>
                     </ListGroup>
                     <Card.Body>
-                        <Card.Link onClick={() => {this.props.handleView('editProduct', this.props.productData)}}>Edit Product</Card.Link>
-                        <Card.Link onClick={() => {this.props.handleDelete(this.props.productData.id)}}>Delete Product</Card.Link>
+                        <Card.Link href="#" onClick={() => {this.props.handleView('editProduct', this.props.productData)}}>Edit Product</Card.Link>
+                        <Card.Link href="#" onClick={() => {this.props.handleDelete(this.props.productData.id)}}>Delete Product</Card.Link>
                     </Card.Body>
                 </Card>
             </div>
