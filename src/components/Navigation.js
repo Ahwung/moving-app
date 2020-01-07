c093807aeb9253a3ecd3aa090ced33fbe7f88894
Adam Hwung <Ahwung@gmail.com>
2020-01-07 14:08:12 -0800

    changed styling for navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,6 +4,10 @@
 // Packages
 import React from 'react';
 import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
 
 // =============================
 // COMPONENT CLASS
@@ -11,17 +15,23 @@ import Nav from 'react-bootstrap/Nav'
 class Navigation extends React.Component {
     render () {
         return (
-            <Nav variant="pills" defaultActiveKey="home">
-                <Nav.Item>
-                    <Nav.Link eventKey="home" onClick={() => {this.props.handleView('homepage')}}>Home</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="table" onClick={() => {this.props.handleView('table')}}>Table</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="add" onClick={() => {this.props.handleView('addProduct')}}>Add New Product</Nav.Link>
-                </Nav.Item>
+            <Navbar bg="dark" variant="dark">
+                <Nav className="mr-auto" defaultActiveKey="home">
+                    <Nav.Item>
+                        <Nav.Link eventKey="home" onClick={() => {this.props.handleView('homepage')}}>Home</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="table" onClick={() => {this.props.handleView('table')}}>Table</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link eventKey="add" onClick={() => {this.props.handleView('addProduct')}}>Add New Product</Nav.Link>
+                    </Nav.Item>
                 </Nav>
+                <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-light">Search</Button>
+                </Form>
+            </Navbar>
         )
     }
 }
